d432a2df3cecc0c0bad8f894ee228a46b3e6bde9
Andrew Tran <tranandrew@gmail.com>
2022-06-08 10:14:25 -0500

    commit not committing. recommitting changes

--- WorkingFolder/src/app.ts
+++ WorkingFolder/src/app.ts
@@ -1,32 +1,17 @@
-//interface describes the structure of an object.
-// you can use interfaces as a type. The variable  assigned that type must follow the interface definition of what the object should be.
-//although you can use interface and type interchangeably, they are not the same.
-// Union types can be stored in types, not interface
-// interface is clearer. when you define it as interface, you want to define the structure of an object with that. This is why interface is used to do this more than custom types. 
-// interface can be implemented in classes. This is why you often use these.
-// class Person implements Greetable
-
-interface Greetable {
+type Admin = {
     name: string;
-
-    greet(phrase: string): void;
+    privilege: string[];
 };
 
-class Person implements Greetable {
-    name: string;
-
-    constructor(n: string) {
-        this.name = n;
-    }
-
-    greet(phrase: string) {
-        console.log(`${phrase} ${this.name}!`)
-    }
+type Employee {
+    name: string; 
+    startDate: Date;
 }
 
-let person1: Greetable;
+type ElevatedEmployee = Admin & Employee;
 
-person1 = new Person("Andrew")
-
-
-person1.greet("What up")
+const e1: ElevatedEmployee = {
+    name: 'Max',
+    privilege: ['create-server'],
+    startDate: new Date()
+}
